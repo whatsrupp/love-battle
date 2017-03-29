d37457682fbd9f11ce1d38563f122affc2f0d090
Nick Rupp <nickrupp95@gmail.com>
2017-03-29 12:29:34 +0100

    Separate /play page load from /names post method

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+    session['player1_name'] = params[:player1_name]
+    session['player2_name'] = params[:player2_name]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
     erb(:play)
   end
 
