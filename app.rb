15af84fb3cb93f90bb799103551efe653b024dd0
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-03-29 11:31:19 +0100

    Entering Player Nick, created feature test for it

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-get '/' do
-  "Testing infrastructure working!"
-end
+  get '/' do
+    erb(:index)
+  end
+
+  post '/names' do
+    @name = params[:name]
+    erb(:play)
+  end
+
 
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,14 @@
 
 feature "Testing Infrastructure" do
-  scenario "Checks that the page says hello" do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
+  # scenario "Checks that the page says hello" do
+  #   visit('/')
+  #   expect(page).to have_content 'Testing infrastructure working!'
+  # end
 
+  scenario "expects players to fill in their names" do
+    visit('/')
+    fill_in('name', :with => 'Nick')
+    click_button('Submit')
+    expect(page).to have_content 'Nick'
+  end
 end
